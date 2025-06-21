7c83296031d8ec1f6e1b115277283e854398e15f
Alisbur <mail-4-eugen@yandex.ru>
2025-06-21 21:29:37 +0300

    autosave 5

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div
+      className={`w-full h-[90vh] flex items-center justify-center text-[24px] md:text-[30px] text-[#334155]`}
+    >
+      404 Страница не найдена
+    </div>
+  );
+}
